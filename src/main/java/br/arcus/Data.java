2ba47dc747852a0ded6bfa64b9e6310dccf6dfb9
BryanSer <390807154@qq.com>
2019-03-31 14:45:06 +0800

    TODO

---
+++ src/main/java/br/arcus/Data.java
@@ -0,0 +1,8 @@
+package br.arcus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Data {
+    public static Map<String,PlayerData> PlayerDatas=  new HashMap<>();
+}
--- src/main/java/br/arcus/SQLManager.java
+++ src/main/java/br/arcus/SQLManager.java
@@ -1,5 +1,13 @@
 package br.arcus;
 
 public class SQLManager {
+    public static PlayerData loadData(String name){
+        //TODO 读取 int long long 剩下我写
 
+        return null;
+    }
+
+    public static void sync(PlayerData data){
+        //TODO 储存int long long
+    }
 }
